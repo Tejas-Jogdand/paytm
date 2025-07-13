90c11dc794f6ca9cf45ba37cd021736a4b8937e5
Tejas Jogdand <tejasjogdand37@gmail.com>
2025-07-13 19:09:54 +0530

    changed backend route & found scope of improvements

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -45,48 +45,48 @@ router.post('/signup', async (req, res) => {
 
     await Account.create({
         userID: userID,
-        balance: 1 + Math.random()* 9999999
+        balance: 1 + Math.random() * 9999999
     })
 
     const token = jwt.sign({
-        userId : userID
-    },process.env.JWT_PASS)
+        userId: userID
+    }, process.env.JWT_PASS)
 
     res.json({
-        message:"User created successfully",
-        token : token
+        message: "User created successfully",
+        token: token
     })
 });
 
 const signinBody = zod.object({
-    username : zod.string().email(),
-    password : zod.string()
+    username: zod.string().email(),
+    password: zod.string()
 })
 
-router.post('/signin',async (req,res)=>{
+router.post('/signin', async (req, res) => {
     const input = signinBody.safeParse(req.body);
-    if(!input.success){
+    if (!input.success) {
         return res.status(411).json({
-            message : "Invalid credentials"
+            message: "Invalid credentials"
         })
     }
 
     const user = await User.findOne({
-        username : input.data.username,
-        password : input.data.password
+        username: input.data.username,
+        password: input.data.password
     })
 
     // console.log(user)
 
-    if(!user){
+    if (!user) {
         return res.status(411).json({
-            message : "Error While loging"
+            message: "Error While loging"
         })
     }
 
     const token = jwt.sign({
-        userId : user._id
-    },process.env.JWT_PASS)
+        userId: user._id
+    }, process.env.JWT_PASS)
 
     res.status(200).json({
         token: token
@@ -96,48 +96,44 @@ router.post('/signin',async (req,res)=>{
 const updateBody = zod.object({
     password: zod.string().min(6).optional(),
     firstName: zod.string().optional(),
-    lastName:zod.string().optional()
+    lastName: zod.string().optional()
 })
 
-router.put('/', authMiddleware ,async (req,res)=>{
+router.put('/', authMiddleware, async (req, res) => {
     const input = updateBody.safeParse(req.body);
-    if(!input.success){
+    if (!input.success) {
         return res.status(411).json({
-            message:"Error while updating information"
+            message: "Error while updating information"
         })
     }
     // console.log(input.data)
     // console.log(req.userId)
     await User.updateOne({
-        _id : req.userId
-    },input.data)
+        _id: req.userId
+    }, input.data)
 
     res.status(200).json({
         message: "Updated successfully"
     })
 })
 
-router.get('/bulk',async (req,res)=>{
+router.get('/bulk', async (req, res) => {
     const filter = req.query.filter || "";
-
-    const users = User.find({
-        $or:[{
-            firstName:{
-                '$regex':filter
-            }
-        },{
-            lastName:{
-                '$regex':filter
-            }
-        }]
-    })
+    
+    //$options: 'i' make case-insensitive
+    const users = await User.find({
+        $or: [
+            { firstName: { $regex: filter, $options: 'i' } },
+            { lastName: { $regex: filter, $options: 'i' } }
+        ]
+    });
 
     res.status(200).json({
-        users : (await users).map(user=>({
+        users: (await users).map(user => ({
             username: user.username,
             firstName: user.firstName,
             lastName: user.lastName,
-            _id:user._id
+            _id: user._id
         }))
     })
 })
